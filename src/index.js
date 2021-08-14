d5d15fea9abb339e922d3d6276e2b9258603923e
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-08-14 18:16:45 +0300

    added hbs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-// import { alert } from '@pnotify/core';
-// import '@pnotify/core/dist/PNotify.css';
-// import '@pnotify/core/dist/BrightTheme.css';
-
-// import debounce from '../node_modules/lodash';
 import fetchCountries from './js/fetchCountries.js';
 import refs from './js/refs.js';
 
-// alert({
-//   type: 'arror',
-//   text: 'success!',
-// });
+const debounce = require('lodash.debounce');
 
-refs.input.addEventListener('input', debounce(handleInput, 2000));
+refs.input.addEventListener('input', debounce(fetchCountries, 2000));
