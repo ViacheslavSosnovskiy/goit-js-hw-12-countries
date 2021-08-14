b299080c469d9dce3010de008f29d148a11b1d29
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-08-14 12:04:10 +0300

    +

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,17 +2,39 @@ import { alert, defaultModules } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import * as PNotifyMobile from '@pnotify/mobile';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
+import '@pnotify/core/dist/BrightTheme.css';
 
 defaultModules.set(PNotifyMobile, {});
 
 alert({
-  text: 'Notice me, senpai!',
+  type: 'success',
+  text: 'success!',
 });
 
 refs = {
-  input: document.querySelector('.js-input'),
+  input: document.getElementById('js-input'),
+  container: document.querySelector('.container'),
 };
 
-refs.input.addEventListener('input', onSuccess);
+refs.input.addEventListener('input', handleInput);
 
-function onSuccess(e) {}
+function handleInput(e) {
+  const name = e.target.value;
+  if (name) {
+    fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+      .then(response => {
+        return response.json;
+      })
+      .then(countries => {
+        const countiesHtml = countries.map(country => `<h4>${country.name}</h4>`).join('');
+
+        // alert({
+        //   type: 'success',
+        //   text: 'Notice me, senpai!',
+        // });
+
+        refs.container.insertAdjecentHTML('beforeend', countiesHtml);
+      })
+      .catch(console.error);
+  }
+}
